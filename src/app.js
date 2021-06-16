6e363c43a33adb638ae96b89859daf4dff943f96
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-06-16 10:41:32 +0200

    linked vuejs

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+    el: "#root",
+    data: {
+        /* icon: <i class="far fa-user"></i>, */
+        counter: 0,
+        navbar: [
+            { link: "Home" },
+            { link: "About" },
+            { link: "Services" },
+            { link: "Process" },
+            { link: "Testimonial" },
+            /* { link: this.icon }, */
+            
+        ],
+        footer: [
+            
+        ]
+    },
+    methods:{}
+})
